357869781877ddec019b309b05d0d1135c2f1d9d
consuelorojas <consuelo.rojas@ing.uchile.cl>
2026-02-05 15:39:22 +0100

    plot scripts for FFTs

---
+++ FFTs/fhn_dyn/parameter_fft/param_plot.py
@@ -0,0 +1,49 @@
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('report.mplstyle')
+
+# Load results
+results = 'FFTs/fhn_dyn/parameter_fft/dyn_fft_20260204_152335.pkl'
+
+with open(results, 'rb') as f:
+    all_results =  pickle.load(f)
+
+
+def export_legend(legend, filename="legend.eps"):
+    fig  = legend.figure
+    fig.canvas.draw()
+    bbox  = legend.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    fig.savefig(filename, dpi="figure", bbox_inches=bbox)
+
+
+# Convert to dataframe
+frame = pd.DataFrame(all_results)
+long_frame = frame.explode('auc', ignore_index=True)
+long_frame['auc'] = long_frame['auc'].astype(float)
+
+# compute mean and std
+frame_stats = (
+    long_frame
+    .groupby('df')['auc']
+    .agg(mean_auc='mean', std_auc='std')
+    .reset_index()
+)
+
+
+# Figure
+plt.figure()
+plt.errorbar(
+    frame_stats['df'],
+    frame_stats['mean_auc'],
+    yerr = frame_stats['std_auc'],
+    fmt='P'
+)
+plt.xlabel(r"Parameter Difference $(\nu - \nu_1)$")
+plt.ylabel('AUC')
+plt.grid(True)
+plt.tight_layout()
+plt.ylim(0.2, 1.1)
+plt.savefig('FFTs/fhn_dyn/parameter_fft/parameter_sweep_auc.pdf')
+plt.show()
---
+++ FFTs/fhn_dyn/periods_fft/period_plot.py
@@ -0,0 +1,51 @@
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('report.mplstyle')
+
+# Load results
+results = 'FFTs/fhn_dyn/periods_fft/dyn_fft_20260205_152932.pkl'
+
+with open(results, 'rb') as f:
+    all_results =  pickle.load(f)
+
+
+def export_legend(legend, filename="legend.eps"):
+    fig  = legend.figure
+    fig.canvas.draw()
+    bbox  = legend.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    fig.savefig(filename, dpi="figure", bbox_inches=bbox)
+
+
+# Convert to dataframe
+frame = pd.DataFrame(all_results)
+long_frame = frame.explode('auc', ignore_index=True)
+long_frame['auc'] = long_frame['auc'].astype(float)
+
+# compute mean and std
+frame_stats = (
+    long_frame
+    .groupby('periods')['auc']
+    .agg(mean_auc='mean', std_auc='std')
+    .reset_index()
+)
+
+
+# Figure
+plt.figure()
+plt.errorbar(
+    frame_stats['periods'],
+    frame_stats['mean_auc'],
+    yerr = frame_stats['std_auc'],
+    fmt='P'
+)
+
+plt.xlabel(r"Number of periods $(N_p)$")
+plt.ylabel("AUC")
+plt.grid(True)
+plt.tight_layout()
+#plt.xticks(frame_stats['periods'].unique()[::2])
+plt.ylim(0.2, 1.1)
+plt.savefig('FFTs/fhn_dyn/periods_fft/period_sweep_auc.pdf')
+plt.show()
